e0aeeb149abfdaaf9c9abfcd1ebed296a3ee3f39
asmaalrawahi <93866936+asmaalrawahi@users.noreply.github.com>
2021-12-25 08:52:56 +0400

    Add files via upload

---
+++ PyProject/compare.py
@@ -0,0 +1,13 @@
+Python 3.7.8 (tags/v3.7.8:4b47a5b6ba, Jun 28 2020, 08:53:46) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+>>> 
+ import math 
+print(" Program to demonstrate the use of pyproject") 
+
+a=30 
+b=20 
+if a> b: 
+  print('a is greater than b') 
+else: 
+  print('b is greater than a') 
